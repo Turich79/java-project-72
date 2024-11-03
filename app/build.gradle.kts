e294b5070d78c0f4d5c25f079b85a73ce991c891
Artur Neustroev <turich79@mail.ru>
2024-11-03 14:35:01 +0700

    step1_4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,9 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
-    checkstyle
     application
-    id("io.freefair.lombok") version "8.6"
 }
 
 application {
@@ -19,13 +20,33 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    //implementation("io.javalin:javalin-rendering:6.1.3")
-    //implementation("io.javalin:javalin:6.1.3")
-    implementation("io.javalin:javalin-rendering:6.3.0")
-    implementation("io.javalin:javalin:6.3.0")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
     implementation("org.slf4j:slf4j-simple:2.0.7")
+//    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("com.konghq:unirest-java:3.13.0")
+    testImplementation("org.assertj:assertj-core:3.24.2")
 }
+
+
 
 tasks.test {
     useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
+
+
+
+
+//tasks.test {
+//    useJUnitPlatform()
+//}

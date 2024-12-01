b5271bd82cbf1c0ac55be87a61cc5756c064d38d
Artur Neustroev <turich79@mail.ru>
2024-12-01 12:43:01 +0700

    final 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     testImplementation("org.assertj:assertj-core:3.25.3")
@@ -36,7 +37,8 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("org.apache.commons:commons-text:1.11.0")
     implementation("gg.jte:jte:3.1.9")
-    implementation("org.slf4j:slf4j-simple:2.0.9")
+//    implementation("org.slf4j:slf4j-simple:2.0.9")
+//    implementation("org.slf4j:slf4j-simple:2.0.13")
     implementation("io.javalin:javalin:6.1.3")
     implementation("io.javalin:javalin-bundle:6.1.3")
     implementation("io.javalin:javalin-rendering:6.1.3")

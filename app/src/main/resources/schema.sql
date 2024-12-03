43153fd0404920bc050be0d2120f73b0a90c3aef
Artur Neustroev <turich79@mail.ru>
2024-12-03 21:28:33 +0700

    final 12 without postgres

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -3,7 +3,8 @@ ALTER TABLE IF EXISTS url_checks DROP CONSTRAINT IF EXISTS fk_url_checks_url_id;
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
+--    id bigint PRIMARY KEY AUTO_INCREMENT,
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) NOT NULL,
     created_at timestamp
 );
@@ -11,7 +12,8 @@ CREATE TABLE urls (
 DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE url_checks (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+--    id bigint PRIMARY KEY AUTO_INCREMENT,
     url_id bigint,
     status_code int,
     h1 varchar(255) NOT NULL,
